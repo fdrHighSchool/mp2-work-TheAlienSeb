cc10d1e59e183218656c0795ef2d852f50978ddf
TheAlienSeb <93489617+TheAlienSeb@users.noreply.github.com>
2022-12-02 12:24:59 -0500

    Create Main.java

---
+++ 02-fizzbuzz/Main.java
@@ -0,0 +1,11 @@
+public static void fizzBuzz(int x){
+    if(x%5 == 0 && x%3 == 0){
+      System.out.println("FizzBuzz");
+}
+      else if(x%5 == 0){
+        System.out.println("Fizz");
+      } else if (x%3 == 0){
+        System.out.println("Buzz");
+        
+      }
+    }
